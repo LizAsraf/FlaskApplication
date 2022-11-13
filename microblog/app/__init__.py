96bbb4b506d17984a30f7cdbc5a20d28f2826e12
Liz Asraf <liz161@gmail.com>
2022-11-13 22:34:49 +0200

    changed the app configuration to fix the enviroment

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from config import Config
 from pymongo import MongoClient
-from dotenv import dotenv_values
+# from dotenv import dotenv_values
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
-db_values = dotenv_values(".env")
-client = MongoClient(host=db_values["DATABASE_URL_APP"], username=db_values["MONGODB_USER"], password=db_values["MONGODB_PASSWORD"])
+# db_values = dotenv_values(".env")
+db_url = os.getenv('DATABASE_URL_APP', default='db:27017')
+db_user = os.getenv('MONGODB_USER', default='root')
+db_password = os.getenv('MONGODB_PASSWORD', default='example') 
+client = MongoClient(host=db_url["DATABASE_URL_APP"], username=db_user["MONGODB_USER"], password=db_password["MONGODB_PASSWORD"])
 db = client['flask_db']
 Users = db['Users']
 Posts = db['Posts']
+
+
 
 from app import routes
