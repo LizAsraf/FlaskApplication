5e887b445a44249a129adaab71f7df2da84f6749
Liz Asraf <liz161@gmail.com>
2022-11-14 22:34:00 +0200

    update tests

--- e2e-test.sh
+++ e2e-test.sh
@@ -8,97 +8,123 @@ do
     sleep 2
 	resp=$(curl http://app:5000/ -Is | grep -c "HTTP/1.1 200")
 done
-##test
-resp=$(curl -X PUT http://localhost:5000/ -Is | grep -c "HTTP/1.1 200")
+
+resp=$(curl -X POST http://localhost:5000/ -Is | grep -c "HTTP/1.1 200")
 if [[ resp -eq 1 ]]; then
 	echo "pass logginpage from / with POST"
 else
 	exit 1
 fi
-##test
-
-resp=$(curl -X POST http://localhost:5000/ -Is | grep -c "HTTP/1.1 200")
-if [[ resp -eq 1 ]]; then
-	echo "pass logginpage from / with POST"
-fi
 #given the localhost url the loging is desplayd, the first page is the login page without entering to the system there is no way to get to the homepage
 resp=$(curl -X POST http://localhost:5000/index -Is | grep -c "HTTP/1.1 200")
 if [[ resp -eq 1 ]]; then	
 	echo "pass logginpage from /index with POST"
+else
+	exit 1
 fi
 
 resp=$(curl http://localhost:5000/index -Is | grep -c "HTTP/1.1 200")
 if [[ resp -eq 1 ]]; then	
 	echo "pass logginpage from /index with GET"
+else
+	exit 1
 fi
 
 resp=$(curl -X POST http://localhost:5000/login -Is | grep -c "HTTP/1.1 200")
 if [[ resp -eq 1 ]]; then
 	echo "pass logginpage from /login with POST"
+else
+	exit 1
 fi
 
 resp=$(curl http://localhost:5000/login -Is | grep -c "HTTP/1.1 200")
 if [[ resp -eq 1 ]]; then
 	echo "pass logginpage from /login"
+else
+	exit 1
 fi
 
 resp=$(curl -X POST http://localhost:5000/api/login/firstname-lastname-username-password -Is | grep -c "HTTP/1.1 302")
 if [[ resp -eq 1 ]]; then
 	echo "pass logginpage from /api/login with POST"
+else
+	exit 1
 fi
 
 resp=$(curl http://localhost:5000/index/liz -Is | grep -c "HTTP/1.1 200")
 if [[ resp -eq 1 ]]; then
 	echo "pass welcome page"
+else
+	exit 1
 fi
 
 resp=$(curl -X POST http://localhost:5000/posts -Is | grep -c "HTTP/1.1 200")
 if [[ resp -eq 1 ]]; then
 	echo "pass create new page with POST"
+else
+	exit 1
 fi
 
 resp=$(curl http://localhost:5000/posts -Is | grep -c "HTTP/1.1 200")
 if [[ resp -eq 1 ]]; then
 	echo "pass create new page"
+else
+	exit 1
 fi
 
 resp=$(curl -X POST http://localhost:5000/api/posts/liztest -Is | grep -c "HTTP/1.1 200")
 if [[ resp -eq 1 ]]; then
 	echo "pass api added new post with POST"
+else
+	exit 1
 fi
 
 resp=$(curl http://localhost:5000/api/posts/liztest -Is | grep -c "HTTP/1.1 200")
 if [[ resp -eq 1 ]]; then
 	echo "pass api added new post"
+else
+	exit 1
 fi
 
 resp=$(curl http://localhost:5000/delete_edit -Is | grep -c "HTTP/1.1 200")
 if [[ resp -eq 1 ]]; then
 	echo "pass delete_edit new page"
+else
+	exit 1
 fi
 
 resp=$(curl -X POST http://localhost:5000/delete_edit -Is | grep -c "HTTP/1.1 200")
 if [[ resp -eq 1 ]]; then
 	echo "pass delete_edit new page with POST"
+else
+	exit 1
 fi
 
 resp=$(curl -X DELETE http://localhost:5000/api/delete/name -Is | grep -c "HTTP/1.1 302")
 if [[ resp -eq 1 ]]; then
 	echo "pass api delete name with DELETE"
+else
+	exit 1
 fi
 
 resp=$(curl http://localhost:5000/delete/name -Is | grep -c "HTTP/1.1 302")
 if [[ resp -eq 1 ]]; then
 	echo "pass delete name with GET"
+else
+	exit 1
 fi
 
 resp=$(curl -X PUT http://localhost:5000/api/update/name/name2 -Is | grep -c "HTTP/1.1 302")
 if [[ resp -eq 1 ]]; then
 	echo "pass api update name to name2 with PUT"
+else
+	exit 1
 fi
 
 
 resp=$(curl http://localhost:5000/update/name/name2 -Is | grep -c "HTTP/1.1 302")
 if [[ resp -eq 1 ]]; then
 	echo "pass update name to name2 with GET"
+else
+	exit 1
 fi
